4e1be9333b8b178d0b49b9c05f86f4ce9fc9095e
atharva-k28 <sdc-user@VY-7F-IOST-030.local>
2025-12-12 15:21:54 +0530

    Added Technical Skills Page only (not connected)

--- CareerGuidanceAppNew/SceneDelegate.swift
+++ CareerGuidanceAppNew/SceneDelegate.swift
@@ -10,23 +10,25 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-
-        let window = UIWindow(windowScene: windowScene)
-        let storyboard = UIStoryboard(name: "MyJourney", bundle: nil)
-        let viewController = storyboard.instantiateInitialViewController()
-
-        window.rootViewController = viewController
-        self.window = window
-        window.makeKeyAndVisible()
+//
+//
+//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        let window = UIWindow(windowScene: windowScene)
+//        let storyboard = UIStoryboard(name: "MyJourney", bundle: nil)
+//        let viewController = storyboard.instantiateInitialViewController()
+//
+//        window.rootViewController = viewController
+//        self.window = window
+//        window.makeKeyAndVisible()
+        
+//UNCOMMENT ABOVE CODE TO RUN ANY STORYBOARD OTHER THAN MAIN!!!!
+    
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
+        //guard let _ = (scene as? UIWindowScene) else { return }
+    //}
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
